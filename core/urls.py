7ffef770758bdf737c36a7e718565709263dcea2
OsiyoComputers <hamidovasadbek09@gmail.com>
2025-03-02 12:19:02 +0500

    search commit

--- core/urls.py
+++ core/urls.py
@@ -1,22 +1,21 @@
-"""
-URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
-
+from main.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello-world/', hello_world),
+    path('today/', today_view),
+    path('talabalar/', talabalar_view),
+    path('talabalar/<int:talaba_id>/', talaba_details_view),
+    path('mualliflar/', mualliflar_view),
+    path('', home_view),
+    path('mualliflar/<int:muallif_id>/', muallif_details_view),
+    path('kitoblar/', kitoblar_view),
+    path('recordlar/', recordlar_view),
+    path('top3-kitoblar/', top3_kitoblar_view)
+
+
+
+
+
 ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,91 @@
 from django.shortcuts import render
+from .models import *
+from django.http import HttpResponse
 
-# Create your views here.
+
+def hello_world(request):
+    return HttpResponse("<h1>Hello World!</h1>")
+
+
+def today_view(request):
+    import datetime
+    today = datetime.date.today()
+    context = {
+        'today': today
+    }
+    return render(request, 'today.html', context)
+
+
+def home_view(request):
+    return render(request, 'home.html')
+
+
+def talabalar_view(request):
+    talabalar = Talaba.objects.all()
+    context = {
+        'talabalar': talabalar
+    }
+    return render(request, 'talabalar.html', context)
+
+
+def talaba_details_view(request, talaba_id):
+    talaba = Talaba.objects.get(id=talaba_id)
+    context = {
+        'talaba': talaba
+    }
+    return render(request, 'talaba_details.html', context)
+
+
+def mualliflar_view(request):
+    mualliflar = Muallif.objects.all()
+    context = {
+        'mualliflar': mualliflar
+    }
+    return render(request, 'mualliflar.html', context)
+
+
+def muallif_details_view(request, muallif_id):
+    muallif = Muallif.objects.get(id=muallif_id)
+    kitoblar = Kitob.objects.filter(muallif=muallif)
+    context = {
+        'muallif': muallif,
+        'kitoblar': kitoblar
+    }
+    return render(request, 'muallif_details.html', context)
+
+
+def kitoblar_view(request):
+    kitoblar = Kitob.objects.all()
+    context = {
+        'kitoblar': kitoblar
+    }
+    return render(request, 'kitoblar_view.html', context)
+
+
+def top3_kitoblar_view(request):
+    kitoblar = Kitob.objects.order_by('-sahifa')[:3]
+    context = {
+        'kitoblar': kitoblar
+    }
+    return render(request, 'top3-kitoblar.html', context)
+
+
+def recordlar_view(request):
+    recordlar = Record.objects.all()
+    context = {
+        'record': recordlar
+    }
+    return render(request, 'recordlar_view.html', context)
+
+
+
+
+
+
+
+
+
+
+
+
+
